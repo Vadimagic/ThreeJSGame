d04de1a9fdf2eba8b2952186830ec77feb65ed0a
Vadim <vadim.prog.2000@gmail.com>
2021-03-18 23:59:37 +0300

    rewrite code

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-// Создал сцену
-const scene = new THREE.Scene();
-
-// Добавил куб на сцену
-const geometry = new THREE.BoxGeometry(3, 1, 3);
-const material = new THREE.MeshLambertMaterial({color: 0xfb8e00});
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.set(0, 0, 0);
-scene.add(mesh);
-
-//Создаём свет
-const ambientLight = new THREE.AmbientLight(0xfffff, .6);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, .6);
-directionalLight.position.set(10, 20, 0);
-scene.add(directionalLight);
-
-//Камера
-const width = 10;
-const height = width * (window.innerHeight / window.innerWidth);
-const camera = new THREE.OrthographicCamera(
-  width / -2,
-  width / 2,
-  height / 2,
-  height / -2,
-  1, 
-  100
-);
-
-camera.position.set(4, 4, 4);
-camera.lookAt(0, 0, 0);
-
-// Рендер
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
-
 document.body.appendChild(renderer.domElement);
+
+let camera, scene, renderer; // Глобальные переменные Three js
+const originalBoxSize = 3;
+
+function init() {
+  scene = new THREE.Scene();
+
+  addLayer(0, 0, originalBoxSize, originalBoxSize);
+
+  addLayer(-10. 0, originalBoxSize, originalBoxSize, 'x');
+
+  const ambientLight = new THREE.AmbientLight(0xfffff, .6);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, .6);
+  directionalLight.position.set(10, 20, 0);
+  scene.add(directionalLight);
+
+  const width = 10;
+  const height = width * (window.innerHeight / window.innerWidth);
+  camera = new THREE.OrthographicCamera(
+    width / -2,
+    width / 2,
+    height / 2,
+    height / -2,
+    1, 
+    100
+  );
+
+  camera.position.set(4, 4, 4);
+  camera.lookAt(0, 0, 0);
+
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+  document.body.appendChild(renderer.domElement);
+}
