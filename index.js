b62a7517a2be5ef6c5a90871c970d68712dc9f4f
Vadim <vadim.prog.2000@gmail.com>
2021-03-22 23:59:19 +0300

    -m

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function init() {
   
   addLayer(-10, 0, originalBoxSize, originalBoxSize, 'x');
   
-  const ambientLight = new THREE.AmbientLight(0xfffff, .6);
+  const ambientLight = new THREE.AmbientLight(0xfffff, 0.6);
   scene.add(ambientLight);
   
-  const directionalLight = new THREE.DirectionalLight(0xffffff, .6);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
   directionalLight.position.set(10, 20, 0);
   scene.add(directionalLight);
   
