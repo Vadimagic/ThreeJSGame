b1718d93b6f8b77d0f6b76ff7f1d34d287cd6583
Vadim <vadim.prog.2000@gmail.com>
2021-03-23 23:57:29 +0300

    create function cut box

--- index.js
+++ index.js
@@ -148,3 +148,24 @@ function animation() {
 
   renderer.render(scene, camera)
 }
+
+function cutBox(topLayer, overlap, size, delta) {
+  const direction = topLayer.direction;
+  const newWidth = direction == 'x' ? overlap : topLayer.width;
+  const newDepth = direction == "z" ? overlap : topLayer.depth;
+
+  topLayer.width = newWidth;
+  topLayer.depth = newWidth;
+
+  topLayer.threejs.scale[direction] = overlap / size;
+  topLayer.threejs.position[direction] -= delta / 2;
+
+  topLayer.cannonjs.position[direction] -= delta / 2;
+
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(newWidth / 2, boxHeight / 2, newDepth / 2)
+  );
+
+  topLayer.cannonjs.shapes = [];
+  topLayer.cannonjs.addShape(shape);
+}
